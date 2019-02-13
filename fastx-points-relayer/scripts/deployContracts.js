8a6ce63b68803dc72c487c01f760726dd7a2ce58
twygod <twygod@gmail.com>
2019-02-13 20:01:51 +0800

    部署脚本修改

---
+++ fastx-points-relayer/scripts/deployContracts.js
@@ -0,0 +1,47 @@
+const config = require('../lib/config/relayer');
+const {getWallets, deployContract} = require('ethereum-waffle');
+const {Wallet, ethers} = require('ethers');
+const fs = require('fs');
+const Clicker = require('../abi/Clicker');
+const Token = require('../abi/Token');
+const FXPoints = require('../abi/FXPoints');
+
+const {jsonRpcUrl,privateKey} = config;
+
+/* eslint-disable no-console */
+class ContractsDeployer {
+  save(filename, _variables) {
+    const variables = Object.entries(_variables)
+      .map(([key, value]) => `  ${key}='${value}'`)
+      .join('\n');
+    fs.writeFile(filename, variables, (err) => {
+      if (err) {
+        return console.error(err);
+      }
+      console.log(`${filename} file updated.`);
+    });
+  }
+
+  async main() {
+    this.provider = new ethers.providers.JsonRpcProvider(jsonRpcUrl);
+    // this.wallets = await getWallets(this.provider);
+    // this.deployer = this.wallets[this.wallets.length - 1];
+    this.deployer = new Wallet(privateKey, this.provider);
+    const clickerContract = await deployContract(this.deployer, Clicker);
+    const tokenContract = await deployContract(this.deployer, Token);
+    const fxPointsContract = await deployContract(this.deployer, FXPoints);
+    const variables = {};
+    variables.CLICKER_CONTRACT_ADDRESS = clickerContract.address;
+    variables.TOKEN_CONTRACT_ADDRESS = tokenContract.address;
+    variables.FXPOINTS_CONTRACT_ADDRESS = fxPointsContract.address;
+    console.log({
+      clickerContract:clickerContract.address,
+      tokenContract:tokenContract.address,
+      fxPointsContract:fxPointsContract.address
+    })
+    //this.save('./.env', variables);
+  }
+}
+
+const prepare = new ContractsDeployer();
+prepare.main();
