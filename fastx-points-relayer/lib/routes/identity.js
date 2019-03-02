92d911c698618270858e5d5752d5449b923e4a2c
twygod <twygod@gmail.com>
2019-03-02 15:12:37 +0800

    请求重试

--- fastx-points-relayer/lib/routes/identity.js
+++ fastx-points-relayer/lib/routes/identity.js
@@ -1,26 +1,61 @@
 import express from 'express';
 import asyncMiddleware from '../middlewares/async_middleware';
 
+const retyCount = 3;
+
+const createAccount = async (identityService, managementKey, ensName) => {
+  for (let i = 1; i<=retyCount; i++){
+    try {
+      const transaction = await identityService.create(managementKey, ensName);
+        break;
+    }catch(err){
+        if(i <= retyCount.count) {
+            console.error("createAccountErr:",i,err)
+        }else{
+            return err;
+        }
+    }
+  }
+}
+
 export const create = (identityService) => async (req, res, next) => {
   const {managementKey, ensName} = req.body;
-  try {
-    const transaction = await identityService.create(managementKey, ensName);
+  const transaction = await createAccount(identityService, managementKey, ensName);
+  console.log({transaction})
+  if(transaction instanceof Error){
+    next(transaction);
+  }else{
     res.status(201)
-      .type('json')
-      .send(JSON.stringify({transaction}));
-  } catch (err) {
-    next(err);
+    .type('json')
+    .send(JSON.stringify({transaction}));
   }
 };
 
+const executeSigned = async (identityService, body) => {
+  for (let i = 1; i<=retyCount; i++){
+    try {
+        accounts = await identityService.executeSigned(body);;
+        break;
+    }catch(err){
+        if(i <= retyCount.count) {
+            console.error("executeSignedErr:",i,err)
+        }else{
+            return err;
+        }
+    }
+  }
+}
+
 export const execution = (identityService) => async (req, res, next) => {
-  try {
-    const transaction = await identityService.executeSigned(req.body);
+  console.log('execution')
+  const transaction = await executeSigned(identityService, req.body);
+  console.log({transaction})
+  if(transaction instanceof Error){
+    next(transaction);
+  }else{
     res.status(201)
-      .type('json')
-      .send(JSON.stringify({transaction}));
-  } catch (err) {
-    next(err);
+    .type('json')
+    .send(JSON.stringify({transaction}));
   }
 };
 
